6d7435aaffe524c082122af8a6e2504028e3d140
Sudhan Bhandari <sudhanbhandari78@gmail.com>
2021-07-29 22:21:22 +0545

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+    echo"<h1>Hello World</h1>";
+    print "BMC";
+?>
+
+
+<?php
+    $a = 78.0;
+    var_dump($a);
+?>
+<h1>Name of student</h1>
+<ul>
+    <li>Sudhan</li>
+    <li>Bikash</li>
+    <li>Himal</li>
+</ul>
+
+<?php
+// $name = "Sudhan";
+// $last_name = "Bhandari";
+// echo $full_name = $name.' '.$last_name;//contatination
+
+$name1 = "Sudhan";
+$lname = "Bhandari";
+$name1.=$lname;
+echo $name1;
+
+
+$table = "<table border = '1px solid black' style = 'width:100%'>
+    <tr>
+        <th>S.N</th>
+        <th>Name</th>
+        <th>College</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Sudhan</td>
+        <td>Avinay</td>
+    </tr>
+";
+echo $table;
+?>
